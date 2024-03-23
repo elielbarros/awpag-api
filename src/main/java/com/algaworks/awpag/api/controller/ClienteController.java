e6cc0c9304afb084eb31db2d352e095203655807
Eliel Silva <elielbarros.silva@gmail.com>
2024-03-22 22:18:28 -0300

    Implementando cadastro de parcelamentos

--- src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/awpag/api/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.algaworks.awpag.api.controller;
 
+import com.algaworks.awpag.domain.exception.NegocioException;
 import com.algaworks.awpag.domain.model.Cliente;
 import com.algaworks.awpag.domain.repository.ClienteRepository;
 import com.algaworks.awpag.domain.service.CadastroClienteService;
@@ -39,7 +40,8 @@ public class ClienteController {
     }
 
     @PutMapping("/{clienteId}")
-    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @Valid @RequestBody Cliente cliente) {
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+                                             @Valid @RequestBody Cliente cliente) {
         if (!clienteRepository.existsById(clienteId)) {
             return ResponseEntity.notFound().build();
         }
@@ -59,6 +61,11 @@ public class ClienteController {
         cadastroClienteService.excluir(clienteId);
 
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<String> capturar(NegocioException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
 }
--- src/main/java/com/algaworks/awpag/domain/service/CadastroClienteService.java
+++ src/main/java/com/algaworks/awpag/domain/service/CadastroClienteService.java
@@ -4,16 +4,20 @@ import com.algaworks.awpag.domain.exception.NegocioException;
 import com.algaworks.awpag.domain.model.Cliente;
 import com.algaworks.awpag.domain.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @AllArgsConstructor
 @Service
 public class CadastroClienteService {
 
     private final ClienteRepository clienteRepository;
+
+    public Cliente buscar(Long clienteId) {
+        return clienteRepository.findById(clienteId)
+                .orElseThrow(() -> new NegocioException(
+                        "Cliente não encontrado."));
+    }
 
     @Transactional // transaction.annotation.Transactional
     public Cliente salvar(Cliente cliente) {
@@ -22,7 +26,8 @@ public class CadastroClienteService {
                 .isPresent();
 
         if (emailEmUso) {
-            throw new NegocioException("Já existe cliente cadastrado com este e-amil");
+            throw new NegocioException("Já existe cliente cadastrado com " +
+                    "este e-amil");
         }
 
         return clienteRepository.save(cliente);
@@ -31,11 +36,6 @@ public class CadastroClienteService {
     @Transactional
     public void excluir(Long clienteId) {
         clienteRepository.deleteById(clienteId);
-    }
-
-    @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<String> capturar(NegocioException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
 }
